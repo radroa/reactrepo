f74ba8c4415be9ae6f1ce1887e5ab35cfd3cd1ae
Rad Roa <shikate21@gmail.com>
2023-11-01 05:46:48 +0800

    Added Link Description

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Then Learn Nothing
+          Learn React Then Learn Nothing On This Platform
         </a>
       </header>
     </div>
