4d0e3bae39cb25a0a0af62a350533fb40772c01e
rad.roa <rad.roa@curogram.com>
2023-11-15 09:50:20 -0800

    Changed Paragraph

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a test edit if this will take effect
         </p>
         <a
           className="App-link"
