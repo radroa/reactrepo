d0783323feab945d1637df86e212c64d13991158
radroa <shikate21@gmail.com>
2024-04-15 13:10:42 +0800

    useDataLoader used with fetching data from json-server API, added AddJobPage.jsx and inserted template.

--- practice-react/src/App.jsx
+++ practice-react/src/App.jsx
@@ -3,12 +3,16 @@ import HomePage from './pages/HomePage.jsx'
 import MainLayout from './layouts/MainLayout.jsx';
 import JobsPage from './pages/JobsPage.jsx';
 import NotFoundPage from './pages/NotFoundPage.jsx';
+import JobPage, { jobLoader } from './pages/JobPage.jsx';
+import AddJobPage from './pages/AddJobPage.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
     <Route index element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
+    <Route path='/add-job' element={<AddJobPage />} />
+    <Route path='/jobs/:id' element={<JobPage />} loader={ jobLoader } />
     <Route path='*' element={<NotFoundPage />} />
   </Route>
   )
